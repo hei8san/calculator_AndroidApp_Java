dd3b152324a8a923b94b6e1f8cd15c85c883cf99
Kohei <kohei.sawabe@gmail.com>
2023-03-09 07:56:30 -0800

    adding clickable and Onclick to first button

--- app/src/main/java/online/ksawa/calculatorapp/MainActivity.java
+++ app/src/main/java/online/ksawa/calculatorapp/MainActivity.java
@@ -3,8 +3,9 @@ package online.ksawa.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.resultTXTView);
+        Button btn = findViewById(R.id.oneBTN);
+        textView.setText(input_digits(btn));
+
+
 
 
 
 
     }
+
+    public String input_digits(View view) {
+        Button btn = findViewById(view.getId());
+
+        return (String) btn.getText();
+    }
+
+    //    protected String input_digits(View view){
+//        switch(view.getId()){
+//            case
+//        }
+//    }
+
 }
