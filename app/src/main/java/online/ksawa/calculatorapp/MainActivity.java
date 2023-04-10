ff0f8401359e8be9913eb0a4b0a7ed28a119b02c
Kohei <kohei.sawabe@gmail.com>
2023-04-09 21:41:40 -0700

    Adding some methods

--- app/src/main/java/online/ksawa/calculatorapp/MainActivity.java
+++ app/src/main/java/online/ksawa/calculatorapp/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public static ArrayList<String> arrayList;
+    public static ArrayList<String> arrayList= new ArrayList<>();;
     public static TextView resultView;
 
-    public static StringBuilder basicNumber;
+    public static int basicNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +21,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         resultView = findViewById(R.id.resultTXTView);
         Button btn;
-        arrayList = new ArrayList<>();
-        basicNumber = new StringBuilder();
-        System.out.println(basicNumber.toString());
+
+        basicNumber = 0;
+
 
     }
 
+//    public void calculate(View view) {
+//        resultView.setText(basicNumber);
+//
+//    }
+
+    public void clearEverything(View view) {
+        arrayList.clear();
+        resultView.setText("");
+        basicNumber = 0;
+    }
 
     public void input_digits(View view) {
         Button btn = findViewById(view.getId());
@@ -37,54 +47,92 @@ public class MainActivity extends AppCompatActivity {
                 switch (view.getId()) {
                     case R.id.oneBTN:
                         arrayList.add("1");
-                        basicNumber.append(1);
+                        if (arrayList.size() == 0) {
+                            basicNumber = 1;
+                        }
                         break;
                     case R.id.twoBTN:
                         arrayList.add("2");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 2;
+                        }
                         break;
                     case R.id.threeBTN:
                         arrayList.add("3");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 3;
+                        }
                         break;
                     case R.id.fourBTN:
                         arrayList.add("4");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 4;
+                        }
                         break;
                     case R.id.fiveBTN:
                         arrayList.add("5");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 5;
+                        }
                         break;
                     case R.id.sixBTN:
                         arrayList.add("6");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 6;
+                        }
                         break;
                     case R.id.sevenBTN:
                         arrayList.add("7");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 7;
+                        }
                         break;
                     case R.id.eightBTN:
                         arrayList.add("8");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 8;
+                        }
                         break;
                     case R.id.nineBTN:
                         arrayList.add("9");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 9;
+                        }
+                        break;
+                    case R.id.zeroBTN:
+                        arrayList.add("0");
+                        if (arrayList.size() == 0) {
+                            basicNumber = 0;
+                        }
                         break;
                     case R.id.addBTN:
-                        arrayList.add("+");
+//                        if (arrayList.size() > 1) {
+                            arrayList.add("+");
+//                        }
                         break;
                     case R.id.equalBTN:
+//                        resultView.setText(basicNumber);
+                        StringBuilder sb = new StringBuilder();
+
+                        for (int i = 0; i < arrayList.size(); i++) {
+                            sb.append(arrayList.get(i));
+                            sb.append(" ");
+                        }
+                        resultView.setText(sb.toString());
+                        break;
 
                     default:
                         arrayList.add("error");
                         break;
                 }
-                StringBuilder sb = new StringBuilder();
 
-                for(int i = 0; i < arrayList.size(); i++){
-                    sb.append(arrayList.get(i));
-
-                }
-                resultView.setText(sb.toString());
             }
         });
 
 
     }
-    public void adding(int a, int b){
+
+    public void adding(int a, int b) {
 
     }
 }
